666698657e2ed40c710f8ad4432733052098033d
svyatoslavbr <borysyuk.svyatoslav@gmail.com>
2025-05-22 12:47:05 +0300

    Update user_.h

--- user_.h
+++ user_.h
@@ -13,7 +13,7 @@ void Order(){
 void Books(){
   clear();
    for(auto it: lbooks.get_map()){
-      cout<<"Id: "<<it.second.get_id()<<"Amout of books : "<<it.second.getAmountOfBooks()<<" Library : "<<libraries.get_by_id(it.second.getLibraryBookId()).get_name()<<" Book type : "<<books.get_by_id(it.second.getBookId()).get_book_type()<<" Name : "<<books.get_by_id(it.second.getBookId()).get_name()<<" Year : "<<books.get_by_id(it.second.getBookId()).get_year()<<" Gener : " << books.get_by_id(it.second.getBookId()).get_gener()<<" Pages :  "<<books.get_by_id(it.second.getBookId()).get_pages()<<" Author : "<<authors.get_by_id(books.get_by_id(it.second.getBookId()).get_author_id()).get_last_name()<<" Publisher : "<<publishers.get_by_id(it.second.getBookId()).get_publisher_id().get_name()<<endl;
+      cout<<"Id: "<<it.second.get_id()<<"Amout of books : "<<it.second.getAmountOfBooks()<<" Library : "<<libraries.get_by_id(it.second.getLibraryBookId()).get_name()<<" Book type : "<<books.get_by_id(it.second.getBookId()).get_book_type()<<" Name : "<<books.get_by_id(it.second.getBookId()).get_name()<<" Year : "<<books.get_by_id(it.second.getBookId()).get_year()<<" Gener : " << books.get_by_id(it.second.getBookId()).get_gener()<<" Pages :  "<<books.get_by_id(it.second.getBookId()).get_pages()<<" Author : "<<authors.get_by_id(books.get_by_id(it.second.getBookId()).get_author_id()).get_last_name()<<" Publisher : "<<publishers.get_by_id(books.get_by_id(it.second.getBookId()).get_publisher_id()).get_name()<<endl;
    }
   
 }
